ce53efacf9b7cf0e1ed4a256ab70bfc944a570fc
Samat <zhumamuratovsamat@gmail.com>
2024-02-01 18:42:42 +0600

    init project

---
+++ src/main/java/kz/agro/agrofarm/AgroFarmApplication.java
@@ -0,0 +1,13 @@
+package kz.agro.agrofarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroFarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgroFarmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/agro/agrofarm/AgroFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.agro.agrofarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroFarmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
