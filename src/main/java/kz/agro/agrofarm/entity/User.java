ca1b80c0902be502051c696a4b71371c5933a212
Samat <zhumamuratovsamat@gmail.com>
2024-03-12 22:14:26 +0500

    change fetch type of roles

--- src/main/java/kz/agro/agrofarm/entity/User.java
+++ src/main/java/kz/agro/agrofarm/entity/User.java
@@ -43,7 +43,7 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
