26e54f8dc00b542d78bf28681d2df0d69c5306d1
Samat <zhumamuratovsamat@gmail.com>
2024-04-16 23:53:02 +0500

    optimize imports

--- src/main/java/kz/agro/agrofarm/controller/GeminiApiController.java
+++ src/main/java/kz/agro/agrofarm/controller/GeminiApiController.java
@@ -6,10 +6,7 @@ import kz.agro.agrofarm.model.response.GeminiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
